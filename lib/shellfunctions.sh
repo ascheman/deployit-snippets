a61a0e03e570a6a13f91d43eb52a5437a2a9f2d9
Gerd Aschemann <gerd@aschemann.net>
2014-01-29 09:31:02 +0100

    Bugfix/Extension: Skip comments and empty lines from RC file

--- lib/shellfunctions.sh
+++ lib/shellfunctions.sh
@@ -1,16 +1,26 @@
 
 # TODO: Check how to return an array to the caller!
 get_environment_from_file () {
+    # The parameters
     local file=$1
     local environment=$2
 
+    # Some constants
+    local comment_re="^\s*\#.*"
+    local emptyline_re="^\s*$"
+    local other_environment_re="^\[.*\]$"
+    
     local environment_found=false
     cat $file | while read line
     do
-        environment_re="^\[$environment\]$"
-        other_environment_re="^\[.*\]$"
         
-        if echo $line | egrep "$environment_re" >/dev/null
+        environment_re="^\[$environment\]$"
+         
+        if echo $line | egrep "$comment_re" >/dev/null
+           then continue
+        elif echo $line | egrep "$emptyline_re" >/dev/null
+           then continue
+        elif echo $line | egrep "$environment_re" >/dev/null
 	       then environment_found=true
 	    elif eval $environment_found
 	       then if echo $line | egrep "$other_environment_re" >/dev/null
--- tst/tst-shellfunctions.sh
+++ tst/tst-shellfunctions.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e
 
-# Set some global variables for current program and directory
+# Set some global variables for current test script and directory
 prog=`basename $0`
 dir=`dirname $0`
 
 source $dir/../lib/shellfunctions.sh
 
-# Usage/Test:
+# TODO: Provide a real unit test for this!
+# Test #1:
 declare params=`get_environment_from_file $dir/../data/tst.config tst`
-echo "Expect '-username heinz -password geheim'"
-echo "Params: '$params'"
+echo "Expect: ' -username heinz -password geheim'"
+echo "Result: '$params'"
+
+# Test #2:
+declare params=`get_environment_from_file $dir/../data/tst.config yyy`
+echo "Expect: ' -username 123 -password 456'"
+echo "Result: '$params'"
