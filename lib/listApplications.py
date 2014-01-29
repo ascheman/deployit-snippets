4dac19dfa5f11d95cbde6f8a20f60d17a9544758
Gerd Aschemann <gerd@aschemann.net>
2014-01-29 17:21:44 +0100

    Search Applications by regex parameter

--- lib/listApplications.py
+++ lib/listApplications.py
@@ -1,12 +1,27 @@
 import sys
 import re
+import os
+
+def trace(msg):
+    if os.environ.has_key("DEPLOYIT_CLI_LOGLEVEL") and os.environ["DEPLOYIT_CLI_LOGLEVEL"] >= 9:
+    	print >> sys.stderr, msg
 
 searchPattern = "Applications/"
 if len(sys.argv) > 1:
     searchPattern = sys.argv[1]
+trace ("Searching by Pattern '" + searchPattern + "'")
 
-print "Searching by Pattern '" + searchPattern + "'"
-apps=repository.search('udm.DeploymentPackage')
-for app in apps:
-    if re.search (searchPattern, app):
-        print app
+pathes=repository.search('udm.DeploymentPackage')
+
+apps = dict()
+for path in pathes:
+    dirname = os.path.dirname(path)
+    trace ("Found '" + dirname + "'")
+    if re.search (searchPattern, dirname):
+    	if apps.has_key(dirname):
+    	    apps[dirname] += 1
+    	else:
+    	    apps[dirname] = 0
+    	
+for appName in sorted(apps.keys()):
+    print appName
