741c2e7b60ae687e99de1240085c286615a534fb
Gerd Aschemann <gerd@aschemann.net>
2014-01-30 17:08:13 +0100

    Moved "listApplications" to tools and enhancing it with exclude pattern

--- lib/listApplications.py
+++ lib/listApplications.py
@@ -1,49 +1,36 @@
 import sys
-import re
 import os
 
 import getopt
 
-from tools import trace
+from tools import *
 
 def usage(progName, exitCode):
     print "usage: " + progName + " -h | [-l] [searchRegex]"
     sys.exit(exitCode)
     	
 def main(argv):    	
-    opts, args = getopt.getopt(sys.argv[1:], "hl")
+    opts, args = getopt.getopt(sys.argv[1:], "hlx:")
     # List version numbers (long listing) also
     listLong = 0
+    excludePattern = None
     for opt, arg in opts:
     	if opt == "-h":
     	    usage(argv[0], 0)
         elif opt == "-l":
             listLong = 1
+        elif opt == "-x":
+            excludePattern = arg
 
     searchPattern = "Applications/"
     if len(args) == 1:
         searchPattern = args[0]
     elif len(args) > 1:
         usage(argv[0], 1)
-        
-    trace ("Searching by Pattern '" + searchPattern + "'")
 
-    pathes=repository.search('udm.DeploymentPackage')
-    
-    apps = dict()
-    for path in pathes:
-        if listLong:
-            name = path
-        else:
-            name = os.path.dirname(path)
-        trace ("Found '" + name + "'")
-        if re.search (searchPattern, name):
-        	if apps.has_key(name):
-        	    apps[name] += 1
-        	else:
-        	    apps[name] = 0
+    appNames = listApplications (repository, listLong, searchPattern, excludePattern)
     	
-    for appName in sorted(apps.keys()):
+    for appName in appNames:
         print appName
         
 # The usual way to start main does not work, obviously the script is embedded somehow?
--- lib/tools.py
+++ lib/tools.py
@@ -1,7 +1,42 @@
 import os
+import re
 import sys
 
 def trace(msg):
     if os.environ.has_key("DEPLOYIT_CLI_LOGLEVEL") and os.environ["DEPLOYIT_CLI_LOGLEVEL"] >= 9:
         print >> sys.stderr, msg
         
+        
+def listApplications(repository, listLong = 0, searchPattern = None, excludePattern = None):
+    pathes=repository.search('udm.DeploymentPackage')
+    
+    trace ("Including pathes with pattern '%s', excluding '%s'" % (searchPattern, excludePattern))
+    # Exclude unwanted patterns
+    if excludePattern:
+    	filteredPathes = []
+    	for path in pathes:
+            trace ("Checking '%s' for exclusion" % path)
+    	    if not re.search (excludePattern, path):
+    	    	filteredPathes.append(path)
+    	pathes = filteredPathes
+
+    # Include wanted pathes	    	
+    if searchPattern:
+        filteredPathes = []
+        for path in pathes:
+            trace ("Checking '%s' for inclusion" % path)
+            if re.search (searchPattern, path):
+            	filteredPathes.append(path)
+        pathes = filteredPathes
+            	
+    if not listLong:
+        apps = dict()
+    	for path in pathes:
+    	    shortName = os.path.dirname(path)
+            if apps.has_key(shortName):
+                apps[shortName] += 1
+            else:
+                apps[shortName] = 0
+        pathes = apps.keys()
+            
+    return sorted(pathes)
