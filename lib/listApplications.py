fd0f21a527e240fc2e56beec3d7092a11e7c3aa0
Gerd Aschemann <gerd@aschemann.net>
2014-01-30 08:34:50 +0100

    Allow for short listings (Application names only) and long listings (inkl. Versions)

--- lib/listApplications.py
+++ lib/listApplications.py
@@ -2,26 +2,44 @@ import sys
 import re
 import os
 
+import getopt
+
 def trace(msg):
     if os.environ.has_key("DEPLOYIT_CLI_LOGLEVEL") and os.environ["DEPLOYIT_CLI_LOGLEVEL"] >= 9:
     	print >> sys.stderr, msg
-
-searchPattern = "Applications/"
-if len(sys.argv) > 1:
-    searchPattern = sys.argv[1]
-trace ("Searching by Pattern '" + searchPattern + "'")
-
-pathes=repository.search('udm.DeploymentPackage')
-
-apps = dict()
-for path in pathes:
-    dirname = os.path.dirname(path)
-    trace ("Found '" + dirname + "'")
-    if re.search (searchPattern, dirname):
-    	if apps.has_key(dirname):
-    	    apps[dirname] += 1
-    	else:
-    	    apps[dirname] = 0
     	
-for appName in sorted(apps.keys()):
-    print appName
+    	
+def main(argv):    	
+    opts, args = getopt.getopt(sys.argv[1:], "l")
+    # List version numbers (long listing) also
+    listLong = 0
+    for opt, arg in opts:
+        if opt == "-l":
+            listLong = 1
+
+    searchPattern = "Applications/"
+    if len(args) > 1:
+        searchPattern = sys.argv[1]
+    trace ("Searching by Pattern '" + searchPattern + "'")
+
+    pathes=repository.search('udm.DeploymentPackage')
+    
+    apps = dict()
+    for path in pathes:
+        if listLong:
+            name = path
+        else:
+            name = os.path.dirname(path)
+        trace ("Found '" + name + "'")
+        if re.search (searchPattern, name):
+        	if apps.has_key(name):
+        	    apps[name] += 1
+        	else:
+        	    apps[name] = 0
+    	
+    for appName in sorted(apps.keys()):
+        print appName
+        
+# if __name__ == "__main__":
+trace ("Calling main(" + str(sys.argv) + ")")
+main(sys.argv)
